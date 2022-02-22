d595f445312809bc8557e8be3825e83416a78e9d
정현수 <jung660317@naver.com>
2022-02-22 16:49:19 +0900

    타입스크립트 입문: 27강 - 33강
    - 인터페이스 섹션 완료

--- 타입스크립트 입문 (인프런)/class-note/3_interface.ts
+++ 타입스크립트 입문 (인프런)/class-note/3_interface.ts
@@ -1,5 +1,62 @@
+// 인터페이스 선언
 interface User {
-  아아아아아아잉: number;
+  age: number;
+  name: string;
 }
 
-const obj: User = { 아아아아아아잉: 1 };
+// 변수 인터페이스 활용
+const hyeonsu: User = {
+  age: 10,
+  name: 'hi',
+};
+
+// 함수 인터페이스 활용
+function getUser(user: User) {
+  console.log(user);
+}
+
+getUser(hyeonsu);
+
+// 함수 스펙(구조)에 인터페이스 활용
+interface SumFunction {
+  (a: number, b: number): number;
+}
+
+var sum: SumFunction;
+sum = function (a: number, b: number): number {
+  return a + b;
+};
+
+// 인터페이스 인덱싱
+interface StringArray {
+  [index: number]: string;
+}
+
+const arr: StringArray = ['a', 'b', 'c'];
+
+// 인터페이스 딕셔너리 패턴
+interface StringRegexDictionary {
+  [key: string]: RegExp;
+}
+
+const obj: StringRegexDictionary = {
+  // something: /abc/,
+  cssFile: /\.css$/,
+  jsFile: /\.js$/,
+};
+
+// 인터페이스 확장
+interface Person {
+  name: string;
+  age: number;
+}
+
+interface Developer extends Person {
+  language: string;
+}
+
+const Hyeonsu: Developer = {
+  name: 'hyeonsu',
+  age: 26,
+  language: 'ts',
+};
